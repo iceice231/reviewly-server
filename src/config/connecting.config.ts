dd38d81c003b5b8c95c19dbace4e70625450b377
iceice <si4ev013@gmail.com>
2025-09-21 18:44:07 +0300

    Иницализация проекта

---
+++ src/config/connecting.config.ts
@@ -0,0 +1,17 @@
+import { TypeOrmModuleOptions} from "@nestjs/typeorm";
+import * as dotenv from "dotenv";
+import * as process from "node:process";
+
+dotenv.config();
+
+export const connectingConfig: TypeOrmModuleOptions = {
+    type: 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT ?? '5432'),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: false,
+    logging: true,
+}
